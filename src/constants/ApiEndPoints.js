434b9389b4d57403f142e66a28e3a2eaebdc522e
sakibkahadi12 <sakib@lamptechs.com>
2026-02-25 21:35:09 +0600

    first commit

--- src/constants/ApiEndPoints.js
+++ src/constants/ApiEndPoints.js
@@ -1,8 +1,10 @@
 export const BASE_URL = "https://middleware.edubarakah.com/api/v1";
 export const ApiEndPoints = {
-    Madrasha:{
-        LIST:"/admin/madrasah/list",
-        SHOW:"/admin/madrasah/show",
-        CREATE_UPDATE:"/admin/madrasah/createUpdate",UPDATE:"",DELETE:""
-    }
-}
+  Madrasha: {
+    LIST: "/admin/madrasah/list",
+    SHOW: "/admin/madrasah/show",
+    CREATE_UPDATE: "/admin/madrasah/createUpdate",
+    UPDATE: "",
+    DELETE: "",
+  },
+};
